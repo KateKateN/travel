95d6f656af33e31bc4574624999fb4ff8763b551
Kate Natochii <katyakatya2736@gmail.com>
2019-06-25 23:35:35 +0300

    adapted for tablet

--- js/main.js
+++ js/main.js
@@ -5,6 +5,34 @@ $(document).ready(function(){
       arrows:true,
       prevArrow: '<div class="lft-arrow"><img src="img/ico/slim-left.png"></div>',
       nextArrow: '<div class="rt-arrow"><img src="img/ico/slim-right.png"></div',
+      responsive: [
+         {
+           breakpoint: 1200,
+           settings: {
+             slidesToShow: 4,
+            //  slidesToScroll: 3,
+            //  infinite: true,
+            //  dots: true
+           }
+         },
+         {
+           breakpoint: 1000,
+           settings: {
+             slidesToShow: 3,
+            //  slidesToScroll: 2
+           }
+         },
+         {
+           breakpoint: 600,
+           settings: {
+             slidesToShow: 2,
+            //  slidesToScroll: 1
+           }
+         }
+         // You can unslick at a given breakpoint now by adding:
+         // settings: "unslick"
+         // instead of a settings object
+       ]
    });
    
    //    $(".bookmark i").mouseenter(function(){
@@ -62,9 +90,122 @@ $(document).ready(function(){
       showSlide();
    });
 
-//animations
-new WOW().init();
+   // animations
+   new WOW().init();
 
+   // map
+   // function initMap(){
+   //    var pos = {lat:49.842786, lng:24.027603};
+   //    var opt = {
+   //       center: pos,
+   //       zoom: 14
+   //       styles: [
+   //          {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+   //          {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+   //          {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+   //          {
+   //            featureType: 'administrative.locality',
+   //            elementType: 'labels.text.fill',
+   //            stylers: [{color: '#d59563'}]
+   //          },
+   //          {
+   //            featureType: 'poi',
+   //            elementType: 'labels.text.fill',
+   //            stylers: [{color: '#d59563'}]
+   //          },
+   //          {
+   //            featureType: 'poi.park',
+   //            elementType: 'geometry',
+   //            stylers: [{color: '#263c3f'}]
+   //          },
+   //          {
+   //            featureType: 'poi.park',
+   //            elementType: 'labels.text.fill',
+   //            stylers: [{color: '#6b9a76'}]
+   //          },
+   //          {
+   //            featureType: 'road',
+   //            elementType: 'geometry',
+   //            stylers: [{color: '#38414e'}]
+   //          },
+   //          {
+   //            featureType: 'road',
+   //            elementType: 'geometry.stroke',
+   //            stylers: [{color: '#212a37'}]
+   //          },
+   //          {
+   //            featureType: 'road',
+   //            elementType: 'labels.text.fill',
+   //            stylers: [{color: '#9ca5b3'}]
+   //          },
+   //          {
+   //            featureType: 'road.highway',
+   //            elementType: 'geometry',
+   //            stylers: [{color: '#746855'}]
+   //          },
+   //          {
+   //            featureType: 'road.highway',
+   //            elementType: 'geometry.stroke',
+   //            stylers: [{color: '#1f2835'}]
+   //          },
+   //          {
+   //            featureType: 'road.highway',
+   //            elementType: 'labels.text.fill',
+   //            stylers: [{color: '#f3d19c'}]
+   //          },
+   //          {
+   //            featureType: 'transit',
+   //            elementType: 'geometry',
+   //            stylers: [{color: '#2f3948'}]
+   //          },
+   //          {
+   //            featureType: 'transit.station',
+   //            elementType: 'labels.text.fill',
+   //            stylers: [{color: '#d59563'}]
+   //          },
+   //          {
+   //            featureType: 'water',
+   //            elementType: 'geometry',
+   //            stylers: [{color: '#17263c'}]
+   //          },
+   //          {
+   //            featureType: 'water',
+   //            elementType: 'labels.text.fill',
+   //            stylers: [{color: '#515c6d'}]
+   //          },
+   //          {
+   //            featureType: 'water',
+   //            elementType: 'labels.text.stroke',
+   //            stylers: [{color: '#17263c'}]
+   //          }
+   //       ]
+   //    };
 
- });
+   //    var map = new google.maps.Map(document.getElementById('map'), opt);
+
+   //    var marker = new google.maps.Marker({
+   //       position: pos,
+   //       map: map,
+   //       title: 'Hello World!',
+   //       icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+   //       animation: google.maps.Animation.DROP,
+   //    });
+   //    marker.addListener('click', toggleBounce)
+
+   //    var info = new google.maps.InfoWindow({
+   //       content: "<h3>This is my plase</h3><p>Simple description</p>"
+   //    });
+   //    marker.addListener('click', function(){
+   //       info.open(map, marker);
+   //    });
+   // }
+   //    function toggleBounce() {
+   //       if (marker.getAnimation() !== null) {
+   //          marker.setAnimation(null);
+   //       } else {
+   //         marker.setAnimation(google.maps.Animation.BOUNCE);
+   //       }
+   //    }
+   // initMap();
+});
  
